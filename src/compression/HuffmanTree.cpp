5c87e1fa6ed1d1426d4a559f238855145ad9774a
Infectiq <finnwillems2002@gmail.com>
2025-07-07 19:03:09 +0200

    Fixed HuffmanCompression and added testCases and /src/tests/ folder

--- src/compression/HuffmanTree.cpp
+++ src/compression/HuffmanTree.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Admin on 05.07.2025.
-//
-
 #include "HuffmanTree.h"
 
 #include <ostream>
@@ -10,28 +6,28 @@
 void HuffmanTree::build(const std::unordered_map<unsigned char, int> &freqMap) {
     //Define lambda comparer func
     auto comp = [](Node* a, Node* b) {
-        return a->freq > b->freq;
+        if (a->freq != b->freq) return a->freq > b->freq;
+        return a->value > b->value; //If frequency is the same, pick smaller value
     };
     //Assign priority queue : DataType, (Store-)Container type, compare operatoor
     std::priority_queue<Node*, std::vector<Node*>, decltype(comp)> queue(comp);
 
     //Assigning NOdes to queue
-    for (auto& [ch, freq] : freqMap) {
+    for (const auto& [ch, freq] : freqMap) {
         queue.push(new Node(ch, freq));
     }
-
     //Building Tree
     while (queue.size() > 1) {
-        Node * n1 = queue.top();
-        queue.pop();
-        Node * n2 = queue.top();
-        queue.pop();
+        Node *n1 = queue.top(); queue.pop();
+        Node *n2 = queue.top(); queue.pop();
+        //Always define wich node is left to achieve determinance
+        if (n1->freq > n2->freq || (n1->freq == n2->freq && n1->value > n2->value))
+            std::swap(n1, n2);
 
         queue.push(new Node(n1, n2));
     }
 
     this->root = queue.top();
-
 }
 
 void HuffmanTree::serialize(std::ostream &out) const {
@@ -39,16 +35,16 @@ void HuffmanTree::serialize(std::ostream &out) const {
 }
 
 void HuffmanTree::serializeNode(Node *node, std::ostream &out) const {
-
     if (!node) return;
 
     unsigned char leafBit = 1;
     unsigned char notLeafBit = 0;
+
     if (node->isLeaf()) {
         out.write(reinterpret_cast<const char *>(&leafBit), 1);
         unsigned char character = node->value;
         out.write(reinterpret_cast<const char *>(&character), sizeof(character));
-    }else {
+    } else {
         out.write(reinterpret_cast<const char *>(&notLeafBit), 1);
         serializeNode(node->left, out);
         serializeNode(node->right, out);
@@ -62,15 +58,17 @@ void HuffmanTree::deserialize(std::istream &in) {
 HuffmanTree::Node *HuffmanTree::deserializeNode(std::istream &in) {
     unsigned char bit;
     in.read(reinterpret_cast<char *>(&bit), 1);
+
     if (bit == 1) {
         unsigned char character;
         in.read(reinterpret_cast<char *>(&character), sizeof(character));
         return new Node(character, 0);
-    }else {
-        return new Node(deserializeNode(in), deserializeNode(in));
+    } else {
+        Node* left = deserializeNode(in);
+        Node* right = deserializeNode(in);
+        return new Node(left, right);
     }
 }
-
 
 //Deleting SubTree to avoid Memory Leak
 void HuffmanTree::deleteSubTree(Node* n) {
@@ -80,8 +78,6 @@ void HuffmanTree::deleteSubTree(Node* n) {
     delete n;
 }
 
-
 HuffmanTree::~HuffmanTree() {
     deleteSubTree(root);
 }
-
