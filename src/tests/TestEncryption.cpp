a97b7567b277fbeb81a4ed8169e6ff120bb742f1
Infectiq <finnwillems2002@gmail.com>
2025-07-18 10:43:52 +0200

    Added Tester for En/De-cryption

---
+++ src/tests/TestEncryption.cpp
@@ -0,0 +1,70 @@
+//
+// Created by Admin on 18.07.2025.
+//
+
+#include <fstream>
+#include <iostream>
+
+#include "../steganography/XOREncryptor.h"
+
+void testEncryptionRoundtrip(const std::filesystem::path& src= "testFile.txt") {
+    //create testfile
+    if (src == "testFile.txt") {
+        std::ofstream createTestFile("testFile.txt");
+        std::string testData = "test123blablablaabcdefghijklmnopqrstuvw\nwasmacheichhier";
+        createTestFile.write(testData.data(), testData.size());
+        createTestFile.close();
+    }
+    //Open file
+    std::ifstream ifs(src, std::ios::binary);
+    //Gather size
+    ifs.seekg(0, std::ios::end);
+    std::streamsize originalSize = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+    //read file
+    std::vector<unsigned char> bufferBefore(originalSize);  //Datencontainer mit keiner festen grösse da size laufzeit bestimmt!
+    ifs.read(reinterpret_cast<char*>(bufferBefore.data()), originalSize);
+    ifs.close();
+    //encrypt file
+    XOREncryptor::encrypt(src, "test");
+    //Open encrypted file
+    std::ifstream ifsEncrypted(src, std::ios::binary);
+    //gather encrypted file size
+    ifsEncrypted.seekg(0, std::ios::end);
+    std::streamsize encryptedSize = ifsEncrypted.tellg();
+    ifsEncrypted.seekg(0, std::ios::beg);
+    //read encrypted file
+    std::vector<unsigned char> encryptedBuffer(encryptedSize);
+    ifsEncrypted.read(reinterpret_cast<char*>(encryptedBuffer.data()), encryptedSize);
+    ifsEncrypted.close();
+    //Decrypt file
+    XOREncryptor::decrypt(src, "test");
+    //Read aftermath
+    std::ifstream ifsDecrypted(src, std::ios::binary);
+    //gather after size
+    ifsDecrypted.seekg(0, std::ios::end);
+    std::streamsize afterSize = ifsDecrypted.tellg();
+    ifsDecrypted.seekg(0, std::ios::beg);
+    //read file again
+    std::vector<unsigned char> bufferAfter(afterSize);
+    ifsDecrypted.read(reinterpret_cast<char*>(bufferAfter.data()), afterSize);
+    ifsDecrypted.close();
+
+    //Check Similarity
+    bool decryptedCorrectly = (originalSize == afterSize && std::equal(bufferBefore.begin(), bufferBefore.end(), bufferAfter.begin()));
+
+    bool encrypted = !std::equal(
+        bufferBefore.begin(),
+        bufferBefore.end(),
+        encryptedBuffer.begin() + 16 //skipping Salt
+    );
+
+    if (encrypted && decryptedCorrectly) {
+        std::cout << "[Info] Test Succesfull!\n";
+    }else {
+        std::cout << "[Info] Test Failed!\n";
+    }
+
+
+
+}
